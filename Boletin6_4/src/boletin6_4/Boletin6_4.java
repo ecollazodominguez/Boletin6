ecbb2492dcb5fe17280d7ebc1fa9c8101a94ce06
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2018-10-30 10:39:44 +0100

    ejercicio4

---
+++ Boletin6_4/src/boletin6_4/Boletin6_4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_4;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Boletin6_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Peso persona1 = new Peso(67, "Maria");
+        Peso persona2 = new Peso(75, "Javier");
+        persona2.CompararPeso(persona1, persona2);
+    }
+    
+}
---
+++ Boletin6_4/src/boletin6_4/Peso.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Peso {
+    int peso;
+    String nome;
+    
+    public Peso(int peso, String nome){
+        this.peso = peso;
+        this.nome = nome;
+    }
+    
+    public void CompararPeso(Peso a, Peso b){
+        int difPeso = 0;
+        if (a.peso > b.peso){
+            difPeso = a.peso - b.peso;
+            JOptionPane.showMessageDialog(null, a.nome+" de peso "+a.peso+" kg, pesa máis ca "+b.nome+" cunha diferencia de "+difPeso+" kg.");
+        } else{
+            difPeso = b.peso - a.peso;
+            JOptionPane.showMessageDialog(null, b.nome+" de peso "+b.peso+" kg, pesa máis ca "+a.nome+" cunha diferencia de "+difPeso+" kg.");
+        }
+    }
+}
