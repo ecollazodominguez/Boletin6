ac8de69250dec892ab1a0ac60a9e7e2cdbf36795
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2018-12-04 12:31:18 +0100

    Ejercicio1

---
+++ Boletin6_7/src/boletin6_7/Superficie.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+
+public class Superficie {
+   private final float PI = 3.14f;
+   private float lado;
+   private float base;
+   private float altura;
+   private float radio;
+   
+    public void calcularArea(){
+        System.out.println("****MENU****\n 1--> Cuadrado\n 2--> Triangulo\n 3--> Circulo\n\n Elige una de las opciones");
+        Scanner sc= new Scanner(System.in);
+        int opcion = sc.nextInt();
+        switch(opcion){
+            case 1:
+                System.out.println("Introduce el lado del cuadrado");
+            Scanner lad= new Scanner(System.in);
+            lado = lad.nextFloat();
+                System.out.println("El área del circulo es de: " +Math.pow(lado, 2)+ "m²");
+                break;
+            
+            case 2:
+                System.out.println("Introduce la base del triángulo:");
+            Scanner bas= new Scanner(System.in);
+            base = bas.nextFloat();
+            
+            System.out.println("Introduce la altura del triángulo:");
+            Scanner altu= new Scanner(System.in);
+            altura = altu.nextFloat();
+                System.out.println("El área del triangulo es de: " + (base * altura)/2  + "m²");
+                break;
+               
+            case 3: System.out.println("Introduce el radio del círculo: ");
+            Scanner rad= new Scanner(System.in);
+            radio = rad.nextFloat();
+                System.out.println("El área del circulo es de: " +PI*Math.pow(radio, 2));
+                break;
+                
+            default : System.out.println("La opción introducida no es correcta.");   
+        }
+    }
+} 
+
